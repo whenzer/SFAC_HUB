795d3afb772b105f094c5353a6061f7bb49fc4ed
whenzer <kled0ng123@gmail.com>
2025-09-19 20:15:52 +0800

    Fix order of middleware usage in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/api/user', userRoutes);
 app.use(express.json());
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => {
   connectUsersDB();
