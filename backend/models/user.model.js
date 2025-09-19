6881b28cb4c135334600734a0a2339eab76163e9
whenzer <kled0ng123@gmail.com>
2025-09-19 20:04:05 +0800

    Fix export statement for User model in user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -10,5 +10,5 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
-
-export default User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+export default User;
