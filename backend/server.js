179ba001608daa2a07be2ad1fe769c9e720716e5
whenzer <kled0ng123@gmail.com>
2025-09-26 10:51:20 +0800

    FIX: localhost api support for frontend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'https://sfac-hub-bq7y.vercel.app',
-    'http://localhost:5173/'
+    'http://localhost:5173'
   ],
   credentials: true 
 }));
