59423187cfa27215dac7f7ba0d9581860dd82451
whenzer <kled0ng123@gmail.com>
2025-09-28 18:59:14 +0800

    ADD: admin heirarchy and exclusive access

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -44,6 +44,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
+        enum: ["admin","teacher","staff", "student"],
         required: true
     },
     reservedItems: [{ 
--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import { adminController } from '../controllers/admin.controller.js';
+import authenticateAdmin from '../middleware/auth.admin.js'
 import { createProduct } from '../controllers/product.controller.js';
 
 const router = express.Router();
+
+router.use(authenticateAdmin);
 
 router.post('/', adminController);
 router.post('/createproduct', createProduct);
